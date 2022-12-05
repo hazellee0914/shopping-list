b8f95bd405ca1e90c651225ec8687087c8e86d34
Hyena Lee <108929456+hazellee0914@users.noreply.github.com>
2022-12-05 17:07:18 +0900

    dubugging

---
+++ main.js
@@ -0,0 +1,59 @@
+const items = document.querySelector('.items');
+const input = document.querySelector('.footer__input');
+const addBtn = document.querySelector('.footer__button');
+
+
+function onAdd() {
+  const text = input.value;
+  console.log(text);
+  if(text === '') {
+    input.focus();
+    return;
+  }
+
+  const item = createItem(text);
+  items.appendChild(item);
+  items.scrollIntoView({ block: 'center' });
+  input.value = '';
+  input.focus();
+}
+
+
+function createItem(text) {
+  const itemRow = document.createElement('li');
+  itemRow.setAttribute('class', 'item__row');
+
+  const item = document.createElement('div');
+  item.setAttribute('class', 'item');
+
+  const name = document.createElement('span');
+  name.setAttribute('class', 'item__name');
+  name.innerHTML = text;
+
+  const deleteBtn = document.createElement('button');
+  deleteBtn.setAttribute('class', 'item__delete');
+  deleteBtn.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+  deleteBtn.addEventListener('click', () => {
+    items.removeChild(itemRow);
+  })
+
+  const itemDivider = document.createElement('div');
+  itemDivider.setAttribute('class', 'item__divider');
+
+  item.appendChild(name);
+  item.appendChild(deleteBtn);
+
+  itemRow.appendChild(item);
+  itemRow.appendChild(itemDivider);
+  return itemRow;
+}
+addBtn.addEventListener('click', () => {
+  onAdd();
+});
+
+input.addEventListener('keypress', event => {
+  // console.log('key');
+  if(event.key === 'Enter') {
+    onAdd();
+  }
+});
